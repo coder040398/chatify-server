b6a62a3707908f4b8c00eb7b812b09ccc7c3c910
Rino <coder040398@gmail.com>
2023-02-15 20:30:38 +0700

    update env for mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import entities from './utils/typeorms';
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.env.development' }), AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env.development' }),
+    AuthModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_DB_HOST,
+      port: parseInt(process.env.MYSQL_DB_PORT),
+      username: process.env.MYSQL_DB_USERNAME,
+      password: process.env.MYSQL_DB_PASSWORD,
+      database: process.env.MYSQL_DB_NAME,
+      synchronize: true,
+      entities,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
